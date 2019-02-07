dc94eedc8bd2bbbfc71781bf598e93c91017e7d9
Bevin Patel <bevin.patel@sparsh-technologies.co.in>
2019-02-07 19:34:03 +0530

    create navigation model for tems of use

---
+++ MVVMRxSwift/MVVMRxSwift/ViewControllers/TemsOfUseRoot/TemsOfUseNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  TemsOfUseNavigationController.swift
+//  MVVMRxSwift
+//
+//  Created by MAC193 on 2/7/19.
+//  Copyright © 2019 MAC193. All rights reserved.
+//
+
+import UIKit
+
+class TemsOfUseNavigationController: BaseNavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ MVVMRxSwift/MVVMRxSwift/ViewControllers/TemsOfUseRoot/TermsOfUseNavigationModel.swift
@@ -0,0 +1,13 @@
+//
+//  TermsOfUseNavigationModel.swift
+//  MVVMRxSwift
+//
+//  Created by MAC193 on 2/7/19.
+//  Copyright © 2019 MAC193. All rights reserved.
+//
+
+import UIKit
+
+class TermsOfUseNavigationModel: BaseNavigationModel {
+
+}
